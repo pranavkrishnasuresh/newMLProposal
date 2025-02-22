f437ee1620e01c9403917924e08af2d8ba64940d
Krishna Suresh <85195581+pranavkrishnasuresh@users.noreply.github.com>
2025-02-21 22:17:17 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'CS 4641 Group 58',
+  description: 'Krishna',
+  generator: 'Krishna',
 }
 
 export default function RootLayout({
